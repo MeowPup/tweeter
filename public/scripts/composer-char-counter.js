6fde3f95e99238f7a85893eb69e69e76a706c5be
EVillett <emily.villett@gmail.com>
2022-10-11 17:07:03 -0700

    added home page with tweets and hover effect

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  
+  $('#tweet-text').keyup(function() {
+    let charCounter = $(this).val().length;
+    let counterVal = 140;
+    let currentChar = counterVal - charCounter;
+    $('.counter').text(currentChar);
+
+    if (currentChar < 0) {
+      $(".counter").addClass("charLimit");
+    } else {
+      $(".counter").removeClass("charLimit");
+    }
+    
+});
+
+});
